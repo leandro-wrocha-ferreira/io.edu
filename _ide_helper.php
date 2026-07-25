5f2caebfc265d2c41f2e2099f55449eb9dded310
Leandro Rocha <leandro.rocha@thinkr.com.br>
2026-07-25 13:06:38 -0300

    chore: add IDE helper for CI3 class autocompletion
    - Create _ide_helper.php with @property stubs on CI_Controller and CI_Model
    - Add empty class stubs for 30 CI3 core classes (DB, Session, Loader, etc.)
    - Remove inline @property from User_model (now inherited via stub)

---
+++ _ide_helper.php
@@ -0,0 +1,107 @@
+<?php
+
+/**
+ * IDE Helper for CodeIgniter 3
+ *
+ * This file is NOT loaded by the application. It exists solely to provide
+ * type resolution and autocompletion for IDEs (PhpStorm, VSCode/Intelephense).
+ *
+ * @package  CodeIgniter
+ * @category  Core
+ */
+
+/**
+ * @property CI_DB_mysqli_driver      $db
+ * @property CI_DB_forge              $dbforge
+ * @property CI_Session               $session
+ * @property CI_Loader                $load
+ * @property CI_Input                 $input
+ * @property CI_URI                   $uri
+ * @property CI_Config                $config
+ * @property CI_Security              $security
+ * @property CI_Form_validation       $form_validation
+ * @property CI_Migration             $migration
+ * @property CI_Lang                  $lang
+ * @property CI_Output                $output
+ * @property CI_Encryption            $encryption
+ * @property CI_Email                 $email
+ * @property CI_User_agent            $agent
+ * @property CI_Table                 $table
+ * @property CI_Pagination            $pagination
+ * @property CI_Upload                $upload
+ * @property CI_Image_lib             $image_lib
+ * @property CI_Zip                   $zip
+ * @property CI_Ftp                   $ftp
+ * @property CI_Javascript            $javascript
+ * @property CI_Typography            $typography
+ * @property CI_Unit_test             $unit_test
+ * @property CI_Cache                 $cache
+ * @property CI_Profiler              $profiler
+ * @property CI_Hooks                 $hooks
+ * @property CI_Log                   $log
+ * @property CI_Router                $router
+ * @property CI_Benchmark             $benchmark
+ */
+class CI_Controller {}
+
+/**
+ * @property CI_DB_mysqli_driver      $db
+ * @property CI_DB_forge              $dbforge
+ * @property CI_Session               $session
+ * @property CI_Loader                $load
+ * @property CI_Input                 $input
+ * @property CI_URI                   $uri
+ * @property CI_Config                $config
+ * @property CI_Security              $security
+ * @property CI_Form_validation       $form_validation
+ * @property CI_Migration             $migration
+ * @property CI_Lang                  $lang
+ * @property CI_Output                $output
+ * @property CI_Encryption            $encryption
+ * @property CI_Email                 $email
+ * @property CI_User_agent            $agent
+ * @property CI_Table                 $table
+ * @property CI_Pagination            $pagination
+ * @property CI_Upload                $upload
+ * @property CI_Image_lib             $image_lib
+ * @property CI_Zip                   $zip
+ * @property CI_Ftp                   $ftp
+ * @property CI_Cache                 $cache
+ */
+class CI_Model {}
+
+// --------------------------------------------------------------------------
+// CI Core Class Stubs (resolve @property types without loading system/)
+// --------------------------------------------------------------------------
+
+class CI_Benchmark {}
+class CI_Cache {}
+class CI_Config {}
+class CI_DB_forge {}
+class CI_DB_mysqli_driver {}
+class CI_Email {}
+class CI_Encryption {}
+class CI_Exceptions {}
+class CI_Form_validation {}
+class CI_Ftp {}
+class CI_Hooks {}
+class CI_Image_lib {}
+class CI_Input {}
+class CI_Javascript {}
+class CI_Lang {}
+class CI_Loader {}
+class CI_Log {}
+class CI_Migration {}
+class CI_Output {}
+class CI_Pagination {}
+class CI_Profiler {}
+class CI_Router {}
+class CI_Security {}
+class CI_Session {}
+class CI_Table {}
+class CI_Typography {}
+class CI_URI {}
+class CI_Unit_test {}
+class CI_Upload {}
+class CI_User_agent {}
+class CI_Zip {}
